19eb2d2d363683d8cdc06786ac647ac882e72ece
TechsupportOnHold <125736044+TechsupportOnHold@users.noreply.github.com>
2024-04-21 19:13:30 +0100

    Add files via upload

---
+++ boot.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+    boot = {
+    # Stay up-to-date on the kernel.
+      kernelPackages = pkgs.linuxPackages_latest;
+      loader = {
+        systemd-boot.editor = false;
+        # Use the systemd-boot EFI boot loader.
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+
+      # Silent Boot
+      kernelParams = [
+        "quiet"
+        "splash"
+        "vga=current"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+      consoleLogLevel = 0;
+    };
+
+    # LUKS prompt
+    boot = {
+      initrd = {
+        systemd.enable = true;
+        verbose = false;
+      };
+      resumeDevice = "/dev/disk/by-uuid/b4f0a7b3-78c1-4c37-9bc5-c01cb1063115";
+      plymouth = {
+        enable = true;
+        theme = "breeze";
+      };
+    };
+    environment.systemPackages = with pkgs; [
+      plymouth
+      kdePackages.breeze-plymouth
+    ];
+}
